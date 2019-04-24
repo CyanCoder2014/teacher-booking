389e3c74d50438f317317001d6911ccf2af9cdf9
CyanCoder2014 <cyancoder2016@gmail.com>
2019-04-25 00:23:39 +0430

    Profile Page added Course request list added

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -535,7 +535,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="copyright">
-                                <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                                <p>CMS Powered by<a href="https://cyancoder.co/en">Cyan Coder</a></p>
                             </div>
                         </div>
                     </div>
