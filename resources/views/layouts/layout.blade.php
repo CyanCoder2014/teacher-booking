5523d1ef4969ad12e2f74ec3b204c7d72ef50fc4
CyanCoder2014 <cyancoder2016@gmail.com>
2019-04-25 03:45:46 +0430

    Profile Page added Course request list added

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Teacher.nu</title>
+    <title>Teacher.nu | learn every thing</title>
+
+    <meta name="description"
+          content="Start learning |
+Choose what you want to learn">
+
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
           integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
