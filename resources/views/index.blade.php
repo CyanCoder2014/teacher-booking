83655d6a33c240967eeef9dc718cdbd6516c3fda
CyanCoder2014 <cyancoder2016@gmail.com>
2019-05-27 15:09:35 +0430

    map added to profile show searching through teachers

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,7 +56,7 @@
 
 
 
-    <div class="    container " style="background-color: #f2f2f2!important;">
+    <div class="    container ">
         <div class="row mt-5 mb-5">
             <div class="col-md-3 bg-white p-3" style="margin-top: 4.5rem">
                 <!--
@@ -411,7 +411,7 @@
         </div>
     </div>
 
-    <div class="container pt-menu"  style="background-color: #f2f2f2!important;">
+    <div class="container pt-menu">
 
 
         <div class="row justify-content-center py-3 mb-5">
